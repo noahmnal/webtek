d7e9280eb7a444fad0e0ee14dc2a35ab209023ab
noahmn <Noahmn@stud.ntnu.no>
2026-09-18 18:36:05 +0200

    Made lamps and bataries work with power

--- gates.js
+++ gates.js
@@ -5,6 +5,8 @@ const tileSize = 30;
 
 
 ctx.fillStyle = "green"
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
 
 let mouseX = 0
 let mouseY = 0
@@ -13,6 +15,7 @@ class box {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.leadPower = false
         boxes.push(this)
     }
 }
@@ -20,20 +23,37 @@ class box {
 class onBox extends box {
     constructor(x, y) {
         super(x, y)
+        this.leadPower = true
     }
     
 }
 
-class lamp  extends box {
+class lamp extends box {
     constructor(x, y) {
         super(x, y)
+        this.leadPower = false
+        this.isGlowing = false
     }
+
+    setIsGlowing(boolean) {
+        this.isGlowing = boolean;
+    }
+
 }
 
 class wire {
     constructor(fromBox, toBox) {
         this.fromBox = fromBox;
         this.toBox = toBox;
+        if (fromBox.leadPower || toBox.leadPower) {
+        this.leadPower = true
+        }
+        if(this.leadPower && (fromBox instanceof lamp)) {
+            fromBox.setIsGlowing(true)
+        }
+        if(this.leadPower && (toBox instanceof lamp)) {
+            toBox.setIsGlowing(true)
+        }
     }
 }
 
@@ -55,6 +75,7 @@ let currentbox = {
 let onBoxes = []
 let lamps = []
 let boxes = []
+let wires = []
 draw();
 
 
@@ -76,20 +97,43 @@ function draw() {
         ctx.fillRect(b.x, b.y, tileSize, tileSize);
     }
 
-    for (b of lamps) {
+    for (l of lamps) {
+        if (l.isGlowing) {
         ctx.fillStyle = "yellow";
-        ctx.fillRect(b.x, b.y, tileSize, tileSize);
+        } else {
+            ctx.fillStyle = "black"
+        }
+        ctx.fillRect(l.x, l.y, tileSize, tileSize);
+    }
+
+    for (w of wires) {
+        
+        ctx.beginPath();
+        ctx.moveTo(w.fromBox.x, w.fromBox.y);   // startpunkt
+        ctx.lineTo(w.toBox.x, w.toBox.y);   // endepunkt
+        ctx.stroke();
     }
 }
 
 let isDragging = false;
 let draggingItem = ''
+let firstSelectedBox = null;
+let secondSelectedBox = null;
 
 
 function checkIfClicked(boxX, boxY, e) {
     return (e.button === 0 && e.offsetX > boxX && e.offsetX < boxX+tileSize &&
         e.offsetY > boxY && e.offsetY < boxY+tileSize)
+}
+
+function checkAnyBlockClicked(e) {
+    for(box of boxes) {
+        if(checkIfClicked(box.x, box.y, e)) {
+            return box;
+        }
     }
+    return null;
+}
 
 
 
@@ -112,20 +156,15 @@ document.addEventListener('mousedown', (e) => {
         draw()
     }
 
-    //Wire from onBoxes
-    for(box of boxes) {
-        if(checkIfClicked(box.x, box.y, e)) {
-            isDragging = true;
-            draggingItem = 'W'
-            draw;
-        }
+    firstSelectedBox = checkAnyBlockClicked(e);
+    console.log(firstSelectedBox)
+    if (firstSelectedBox !== null) {
+        isDragging = true;
+        draggingItem = 'W'
+        draw;
     }
 
-
-
 });
-
-
     document.addEventListener('mousemove', (e) => {
         if(isDragging) {
             mouseX = e.offsetX
@@ -138,18 +177,28 @@ document.addEventListener('mousedown', (e) => {
 });
 
 document.addEventListener('mouseup', (e) => {
+    console.log(isDragging)
     if(e.button === 0 && isDragging === true) {
+
         isDragging = false
         console.log(draggingItem);
         switch (draggingItem) {
-
             case 'O':
                 onBoxes.push(new onBox(mouseX, mouseY));
                 break;
             case 'L':
                 lamps.push(new lamp(mouseX, mouseY));
                 break;
+            case 'W':
+                secondSelectedBox = checkAnyBlockClicked(e);
+                if(secondSelectedBox !== null) {
+                    wires.push(new wire(firstSelectedBox, secondSelectedBox))
+                    console.log("WIREE")
+                
+                }
+                break;
         }
+        draw()
             
     }
 });
